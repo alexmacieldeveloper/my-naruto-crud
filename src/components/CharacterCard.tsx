d01e0be5cb9575a7225b9c77a6cd0065bbacad2a
Álex Maciel <aweasley.gina@gmail.com>
2025-03-28 17:54:00 -0300

    adicionando toggle de personagem favorito no card

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -2,11 +2,27 @@ import { Character } from "../types/Character";
 
 interface CharacterCardProps {
   character: Character;
+  toggleFavorite: () => void; 
+  isFavorite: boolean;
 }
 
-const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
+const CharacterCard: React.FC<CharacterCardProps> = ({
+  character,
+  toggleFavorite,
+  isFavorite,
+}) => {
   return (
     <div className="bg-white shadow-md p-4 rounded-lg">
+      <div className="flex justify-between items-center">
+        <button
+          onClick={toggleFavorite} 
+          className={`text-xl ${
+            isFavorite ? "text-yellow-500" : "text-gray-300"
+          }`}
+        >
+          {isFavorite ? "★" : "☆"} 
+        </button>
+      </div>
       {character.images && character.images.length > 0 && (
         <img
           src={character.images[0]}
@@ -15,7 +31,6 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
         />
       )}
       <h2 className="text-lg font-bold">{character.name}</h2>
-      <p>{character.personal.clan}</p>
     </div>
   );
 };
