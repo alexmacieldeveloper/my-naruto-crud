f22e6b88f850f8faf29dc36fc04f03ecb574b8b4
Álex Maciel <aweasley.gina@gmail.com>
2025-03-28 18:02:58 -0300

    importando componets FilterModal, pasando logica do filtr, busca

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { getCharacters } from './services/api';
-import { Character } from './types/Character';
-import CharacterCard from './components/CharacterCard';
+import FilterModal from "./components/ModalFilter";
 
 const App: React.FC = () => {
   const [characters, setCharacters] = useState<Character[]>([]);
@@ -23,6 +20,51 @@ console.log('data', characters)
     };
     fetchCharacters();
   }, []);
+  const filterCharacters = () => {
+    let filtered = characters;
+
+    if (searchTerm) {
+      filtered = filtered.filter((character) =>
+        character.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+
+    if (selectedClan) {
+      filtered = filtered.filter((character) => {
+        const clan = character.personal?.clan;
+
+        if (Array.isArray(clan)) {
+          return clan.some((clanItem) =>
+            clanItem.toLowerCase().includes(selectedClan.toLowerCase())
+          );
+        } else if (typeof clan === "string") {
+          return clan.toLowerCase().includes(selectedClan.toLowerCase());
+        }
+        return false;
+      });
+    }
+
+    if (selectedVillage) {
+      filtered = filtered.filter((character) =>
+        character.personal?.affiliation?.some((village) =>
+          village.toLowerCase().includes(selectedVillage.toLowerCase())
+        )
+      );
+    }
+
+    if (selectedGender) {
+      filtered = filtered.filter(
+        (character) =>
+          character.personal.sex?.toLowerCase() === selectedGender.toLowerCase()
+      );
+    }
+
+    setFilteredCharacters(filtered);
+  };
+
+  useEffect(() => {
+    filterCharacters();
+  }, [searchTerm, selectedClan, selectedVillage, selectedGender]);
 
   if (loading) {
     return <div className="p-6 text-center">Carregando...</div>;
@@ -30,7 +72,32 @@ console.log('data', characters)
 
   return (
     <div className="p-6 text-center">
-      <h1 className="text-2xl font-bold mb-4 text-orange-500">Personagens de Naruto</h1>
+      <div className="flex justify-evenly mb-6">
+        <input
+          type="text"
+          className="search-input p-2 border rounded w-1/2"
+          placeholder="Buscar personagem..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="bg-orange-500 text-white p-2 rounded w-24"
+        >
+          Filtrar
+        </button>
+      </div>
+      <FilterModal
+        isOpen={isModalOpen}
+        closeModal={() => setIsModalOpen(false)}
+        selectedClan={selectedClan}
+        setSelectedClan={setSelectedClan}
+        selectedVillage={selectedVillage}
+        setSelectedVillage={setSelectedVillage}
+        selectedGender={selectedGender}
+        setSelectedGender={setSelectedGender}
+        applyFilters={filterCharacters}
+      />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {characters.map((character) => (
             <CharacterCard key={character.id} character={character} />
