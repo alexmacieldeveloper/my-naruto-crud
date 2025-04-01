f355c5de4e043cecfee119a68d0776bbbc4b3911
Álex Maciel <aweasley.gina@gmail.com>
2025-03-31 22:58:17 -0300

    adicionando um footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="text-center py-4 mt-8 bg-orange-500 text-white">
+        <p>Desenvolvido por Álex Maciel - 2025</p>
+      </footer>
+    );
+  };
+
+export default Footer;
